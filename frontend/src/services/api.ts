35583405d3e1b468aad29f59eb6859dbecc32cf3
fyorina21 <fyorinegasiabriha@gmail.com>
2026-07-05 17:21:18 -0300

    edited

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://online-merkato-zorr.onrender.com/',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
